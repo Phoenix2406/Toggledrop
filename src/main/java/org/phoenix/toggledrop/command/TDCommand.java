bcaa7424766fa39af3f53484a0794a32b336f1e5
Phoenix2406 <coding.projects24@gmail.com>
2025-01-13 07:38:30 +0700

    Added code to check items filter

--- src/main/java/org/phoenix/toggledrop/command/TDCommand.java
+++ src/main/java/org/phoenix/toggledrop/command/TDCommand.java
@@ -1,5 +1,27 @@
+/**
+ * LICENSE
+ * Toggledrop for prevent items being dropped
+ * Copyright (C) 2025 Phoenix
+ * ----------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * END
+ */
+
 package org.phoenix.toggledrop.command;
 
+import org.bukkit.Material;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.phoenix.toggledrop.Main;
 import org.phoenix.toggledrop.utils.ColorCodeTranslate;
 import org.bukkit.Bukkit;
@@ -11,32 +33,28 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
-import java.util.HashSet;
-import java.util.UUID;
+import java.util.*;
 
 public class TDCommand implements CommandExecutor, Listener {
 
-    private Main plugin;
+    private final Main plugin;
+    private final HashSet<UUID> playerToggle = new HashSet<>();
 
     public TDCommand(Main plugin) {
         this.plugin = plugin;
-        plugin.getCommand("toggledrop").setExecutor(this);
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    HashSet<UUID> selfToggle = new HashSet<>();
-    HashSet<UUID> playerToggle = new HashSet<>();
-
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, String label, @NonNull String[] args) {
         if (label.equalsIgnoreCase("toggledrop") || label.equalsIgnoreCase("td")) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cSorry! Only player can execute this command."));
+                sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cSorry! Only player can execute this command."));
                 return true;
             }
         }
 
-        Player p = (Player) sender;
+        Player player = (Player) sender;
 
         if (args.length == 0) {
             sender.sendMessage(ColorCodeTranslate.chat(""));
@@ -47,72 +65,68 @@ public class TDCommand implements CommandExecutor, Listener {
 
         if (args.length >= 1) {
             if (args[0].equalsIgnoreCase("help")) {
-                if (p.isOp() || p.hasPermission("toggledrop.help")) {
-                    sender.sendMessage(ColorCodeTranslate.chat("&#00E6FF&l&m------------&r&#00E6FF&lToggledrop &#00E6FF&l&m------------"));
+                if (player.isOp() || player.hasPermission("toggledrop.help")) {
+                    sender.sendMessage(ColorCodeTranslate.chat("&#00E6FF&l&m------------&r &#00E6FF&lToggledrop &#00E6FF&l&m------------"));
                     sender.sendMessage(ColorCodeTranslate.chat(""));
-                    sender.sendMessage(ColorCodeTranslate.chat("&d/giftaway help &f▶ &7To see all command in this plugin."));
-                    sender.sendMessage(ColorCodeTranslate.chat("&d/giftaway toggle [player] &f▶ &7To turn ON/OFF your or other player toggledrop."));
-                    sender.sendMessage(ColorCodeTranslate.chat("&d/giftaway reload &f▶ &7To reload this plugin."));
-                    sender.sendMessage(ColorCodeTranslate.chat("&d/giftaway version &f▶ &7See version of this plugin."));
+                    sender.sendMessage(ColorCodeTranslate.chat("&d/toggledrop help &f▶ &7To see all command in this plugin."));
+                    sender.sendMessage(ColorCodeTranslate.chat("&d/toggledrop toggle [player] &f▶ &7To turn ON/OFF your or other player drop."));
+                    sender.sendMessage(ColorCodeTranslate.chat("&d/toggledrop reload &f▶ &7To reload this plugin."));
+                    sender.sendMessage(ColorCodeTranslate.chat("&d/toggledrop version &f▶ &7See current version of this plugin."));
                     sender.sendMessage(ColorCodeTranslate.chat(""));
-                    sender.sendMessage(ColorCodeTranslate.chat("&#00E6FF&l&m--------------------------------"));
+                    sender.sendMessage(ColorCodeTranslate.chat("&#00E6FF&l&m-------------------------------------"));
                 } else {
-                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cYou need permission toggledrop.help to use this command."));
+                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cYou need permission toggledrop.help to use this command."));
                 }
             } else if (args[0].equalsIgnoreCase("toggle")) {
-                if (p.isOp() || p.hasPermission("toggledrop.toggle")) {
-                    if (plugin.getConfig().getStringList("disableWorld").contains(p.getWorld().getName())){
-                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + plugin.getMessagesConfig().getString("messages.disableWorld")));
+                if (player.hasPermission("toggledrop.toggle")) {
+                    if (args.length != 2) {
+                        if (!playerToggle.contains(player.getUniqueId())) {
+                            playerToggle.add(player.getUniqueId());
+                            sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " " + plugin.getMessagesConfig().getString("messages.toggleEnable.selfToggle")));
+                        } else if (playerToggle.contains(player.getUniqueId())) {
+                            playerToggle.remove(player.getUniqueId());
+                            sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " " + plugin.getMessagesConfig().getString("messages.toggleDisable.selfToggle")));
+                        }
                     } else {
-                        if (args.length != 2) {
-                            if (!selfToggle.contains(p.getUniqueId())) {
-                                selfToggle.add(p.getUniqueId());
-                                sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + plugin.getMessagesConfig().getString("messages.toggleEnable.selfToggle")));
-                            } else if (selfToggle.contains(p.getUniqueId())) {
-                                selfToggle.remove(p.getUniqueId());
-                                sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + plugin.getMessagesConfig().getString("messages.toggleDisable.selfToggle")));
-                            }
-                        } else {
-                            if (p.isOp() || p.hasPermission("toggledrop.toggle.other")) {
-                                Player target = Bukkit.getPlayer(args[1]);
+                        if (player.isOp() || player.hasPermission("toggledrop.toggle.other")) {
+                            Player target = Bukkit.getPlayer(args[1]);
 
-                                if (Bukkit.getPlayer(args[1]) != null) {
-                                    if (!playerToggle.contains(target.getUniqueId())) {
-                                        playerToggle.add(target.getUniqueId());
-                                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + plugin.getMessagesConfig().getString("messages.toggleEnable.playerToggle") + args[1] + "&a!"));
-                                    } else if (playerToggle.contains(target.getUniqueId())) {
-                                        playerToggle.remove(target.getUniqueId());
-                                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + plugin.getMessagesConfig().getString("messages.toggleDisable.playerToggle") + args[1] + "&a!"));
-                                    }
-                                } else {
-                                    p.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cSorry! Player " + args[1] + " is not online."));
+                            if (target != null) {
+                                if (!playerToggle.contains(target.getUniqueId())) {
+                                    playerToggle.add(target.getUniqueId());
+                                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " " + plugin.getMessagesConfig().getString("messages.toggleEnable.playerToggle") + args[1] + "&a!"));
+                                } else if (playerToggle.contains(target.getUniqueId())) {
+                                    playerToggle.remove(target.getUniqueId());
+                                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " " + plugin.getMessagesConfig().getString("messages.toggleDisable.playerToggle") + args[1] + "&a!"));
                                 }
                             } else {
-                                sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cYou need permission toggledrop.toggle.other to use this command."));
+                                sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cSorry! Player " + args[1] + " is not online."));
                             }
+                        } else {
+                            sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cYou need permission toggledrop.toggle.other to use this command."));
                         }
                     }
                 } else {
-                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cYou need permission toggledrop.toggle to use this command."));
+                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cYou need permission toggledrop.toggle to use this command."));
                 }
             } else if (args[0].equalsIgnoreCase("reload")) {
-                if (p.isOp() || p.hasPermission("toggledrop.reload")) {
+                if (player.isOp() || player.hasPermission("toggledrop.reload")) {
                     if (!plugin.getConfig().getString("version").equals(Main.getInstance().getVersion())) {
-                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cPlugin can't reload properly. Try to see console for more information!"));
-                        throw new RuntimeException("Plugin version is corrupted on config.yml");
+                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cPlugin can't reload properly. Try to see console for more information!"));
+                        throw new IllegalArgumentException("Plugin version is corrupted on config.yml");
                     } else {
-                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&aPlugin successfully reloaded!"));
+                        sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &aPlugin successfully reloaded!"));
                         plugin.saveDefaultConfig();
                         plugin.reloadConfig();
                     }
                 } else {
-                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cYou need permission toggledrop.reload to use this command."));
+                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cYou need permission toggledrop.reload to use this command."));
                 }
             } else if (args[0].equalsIgnoreCase("version")) {
-                if (p.isOp() || p.hasPermission("toggledrop.version")) {
-                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&aThis plugin running on &b&nversion " + Main.getInstance().getVersion()));
+                if (player.isOp() || player.hasPermission("toggledrop.version")) {
+                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &aThis plugin running on &b&nversion " + Main.getInstance().getVersion()));
                 } else {
-                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + "&cYou need permission toggledrop.version to use this command."));
+                    sender.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " &cYou need permission toggledrop.version to use this command."));
                 }
             }
         }
@@ -121,13 +135,26 @@ public class TDCommand implements CommandExecutor, Listener {
 
     @EventHandler
     public void onItemDrop(PlayerDropItemEvent e) {
-        Player p = e.getPlayer();
+        Player player = e.getPlayer();
+        Set<Material> materialList = new HashSet<>();
 
-        if (selfToggle.contains(p.getUniqueId()) || playerToggle.contains(p.getUniqueId())) {
-            p.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + plugin.getMessagesConfig().getString("messages.cantDrop")));
-            e.setCancelled(true);
-        } else if (!selfToggle.contains(p.getUniqueId()) || !playerToggle.contains(p.getUniqueId())) {
-            e.setCancelled(false);
+        if (!plugin.getConfig().getStringList("item-filter.list").isEmpty()) {
+            for (String rawItem : plugin.getConfig().getStringList("item-filter.list")) {
+                Material material = Material.matchMaterial(rawItem);
+                materialList.add(material);
+            }
+        }
+
+        if (playerToggle.contains(player.getUniqueId())) {
+            if (!plugin.getConfig().getStringList("disableWorld").contains(player.getWorld().getName())) {
+                if (plugin.getConfig().getString("item-filter.type").toUpperCase().equals("EXCLUDE") &&
+                        !materialList.contains(e.getItemDrop().getItemStack().getType()) ||
+                        plugin.getConfig().getString("item-filter.type").toUpperCase().equals("INCLUDE") &&
+                                materialList.contains(e.getItemDrop().getItemStack().getType()) || materialList.isEmpty()) {
+                    player.sendMessage(ColorCodeTranslate.chat(plugin.getConfig().getString("prefix") + " " + plugin.getMessagesConfig().getString("messages.cantDrop")));
+                    e.setCancelled(true);
+                }
+            }
         }
     }
 }
